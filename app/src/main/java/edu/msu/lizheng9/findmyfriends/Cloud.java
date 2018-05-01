8a815a523b850d48148891e2f779d93cde5d87cf
TubbyTiger <krntime@gmail.com>
2018-04-30 22:54:50 -0400

    login

---
+++ app/src/main/java/edu/msu/lizheng9/findmyfriends/Cloud.java
@@ -0,0 +1,90 @@
+package edu.msu.lizheng9.findmyfriends;
+
+import android.util.Log;
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Kevin on 4/30/2018.
+ */
+
+public class Cloud {
+    private static String SET_USER_URL = "http://webdev.cse.msu.edu/~lizheng9/cse476/project3/login.php";
+
+    public static void logStream(InputStream stream) {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(stream));
+
+        Log.e("476", "logStream: If you leave this in, code after will not work!");
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Log.e("476", line);
+            }
+        } catch (IOException ex) {
+            return;
+        }
+    }
+
+
+    public boolean User(String username, String deviceId){
+        if((username.length() ==0 )){
+            return false;
+        }
+        String query, url_ = "";
+        query = SET_USER_URL + "?user=" + username + "&device=" + deviceId;
+
+        InputStream stream = null;
+        try {
+            URL url = new URL(query);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            int responseCode = conn.getResponseCode();
+            if(responseCode != HttpURLConnection.HTTP_OK) {
+                return false;
+            }
+
+            stream = conn.getInputStream();
+          //  logStream(stream);
+        } catch (MalformedURLException e) {
+            // Should never happen
+            return false;
+        } catch (IOException ex) {
+            return false;
+        }
+
+        try {
+            XmlPullParser xml = Xml.newPullParser();
+            xml.setInput(stream, "UTF-8");
+
+            xml.nextTag();      // Advance to first tag
+            xml.require(XmlPullParser.START_TAG, null, "fmf");
+
+            String status = xml.getAttributeValue(null, "status");
+            if(status.equals("no")) {
+                return false;
+            }
+            // We are done
+        } catch(XmlPullParserException ex) {
+            return false;
+        } catch(IOException ex) {
+            return false;
+        }finally {
+            try {
+                stream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
+    }}
--- app/src/main/java/edu/msu/lizheng9/findmyfriends/MainActivity.java
+++ app/src/main/java/edu/msu/lizheng9/findmyfriends/MainActivity.java
@@ -1,7 +1,16 @@
 package edu.msu.lizheng9.findmyfriends;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public boolean userCloud = false;
+    public void onGoClicked(final View view){
+       final String android_id = Settings.Secure.getString(getContentResolver(),
+               Settings.Secure.ANDROID_ID);
+
+       final EditText editUsername = findViewById(R.id.editUsername);
+       final String username = editUsername.getText().toString();
+       Thread t = new Thread(new Runnable() {
+           @Override
+           public void run() {
+               Cloud cloud = new Cloud();
+               userCloud = cloud.User(username,android_id);
+
+               view.post(new Runnable() {
+                   @Override
+                   public void run() {
+                       if (!userCloud){
+                           Toast.makeText(view.getContext(),
+                                   R.string.user_fail,
+                                   Toast.LENGTH_SHORT).show();
+                       }
+                       else{
+                           Toast.makeText(view.getContext(),
+                                   R.string.user_success,
+                                   Toast.LENGTH_SHORT).show();
+                       }
+                   }
+               });
+           }
+       });
+       t.start();
+       try { t.join(3000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+        if(userCloud){
+            Intent intent = new Intent(this,MapsActivity.class);
+            intent.putExtra("username",username);
+            intent.putExtra("device",android_id);
+            startActivity(intent);
+        }
+
+    }
+
 }
